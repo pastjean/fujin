9324c9ccf31714aa2362a5ca36085365e1fa610b
Pierre-Alexandre St-Jean <pa@stjean.me>
2013-03-14 17:41:41 -0400

    They really forgot some SPI 2 things
    define fix_p33EP512MC806.h

---
+++ firmware/dspic/fix_p33EP512MC806.h
@@ -0,0 +1,55 @@
+// Thoses microchips guys forgot some things in their definition
+// of the p33EP512MC806.h
+
+#ifndef FIX_P33EP512MC806_H
+#define	FIX_P33EP512MC806_H
+
+extern volatile unsigned int  RPINR22 __attribute__((__sfr__));
+__extension__ typedef struct tagRPIsNR22BITS {
+  union {
+    struct {
+      unsigned SDI2R:7;
+      unsigned :1;
+      unsigned SCK2R:7;
+    };
+    struct {
+      unsigned SDI2R0:1;
+      unsigned SDI2R1:1;
+      unsigned SDI2R2:1;
+      unsigned SDI2R3:1;
+      unsigned SDI2R4:1;
+      unsigned SDI2R5:1;
+      unsigned SDI2R6:1;
+      unsigned :1;
+      unsigned SCK2R0:1;
+      unsigned SCK2R1:1;
+      unsigned SCK2R2:1;
+      unsigned SCK2R3:1;
+      unsigned SCK2R4:1;
+      unsigned SCK2R5:1;
+      unsigned SCK2R6:1;
+    };
+  };
+} RPINR20BITS;
+extern volatile RPINR20BITS RPINR22bits __attribute__((__sfr__));
+
+/* RPINR20 */
+#define _SDI2R RPINR22bits.SDI2R
+#define _SCK2R RPINR22bits.SCK2R
+#define _SDI2R0 RPINR22bits.SDI2R0
+#define _SDI2R1 RPINR22bits.SDI2R1
+#define _SDI2R2 RPINR22bits.SDI2R2
+#define _SDI2R3 RPINR22bits.SDI2R3
+#define _SDI2R4 RPINR22bits.SDI2R4
+#define _SDI2R5 RPINR22bits.SDI2R5
+#define _SDI2R6 RPINR22bits.SDI2R6
+#define _SCK2R0 RPINR22bits.SCK2R0
+#define _SCK2R1 RPINR22bits.SCK2R1
+#define _SCK2R2 RPINR22bits.SCK2R2
+#define _SCK2R3 RPINR22bits.SCK2R3
+#define _SCK2R4 RPINR22bits.SCK2R4
+#define _SCK2R5 RPINR22bits.SCK2R5
+#define _SCK2R6 RPINR22bits.SCK2R6
+
+#endif	/* FIX_P33EP512MC806_H */
+
