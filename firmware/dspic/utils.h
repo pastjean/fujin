c8d27cdcb33aac15aae017d61db9b22d6ced47e2
Pierre-Alexandre St-Jean <pa@stjean.me>
2013-05-16 12:52:34 -0400

    Added some utility functions

---
+++ firmware/dspic/utils.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   utils.h
+ * Author: pastjean
+ *
+ * Created on May 16, 2013, 12:29 PM
+ */
+
+#ifndef UTILS_H
+#define	UTILS_H
+
+#include "math.h"
+
+void clear_buf(void* buf,size_t len){
+    memset(buf,0,len);
+}
+
+
+#define PRECISE_PI (asin(1)*2.0)
+#define PI 3.1415926536
+
+static inline float deg2rad(float deg){return (deg*2.0*PRECISE_PI)/360;}
+static inline float rad2deg(float rad){return (rad*180.0)/PRECISE_PI;}
+#endif	/* UTILS_H */
+
